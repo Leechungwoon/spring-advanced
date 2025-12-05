efeb6d149b7409f82fb70bc29c87d8df731e523f
Leechungwoon <dlcjddns22@gmail.com>
2025-12-05 09:34:36 +0900

    ✨ feat: Lv3 N+1문제
    레벨 3
    - N+1문제
    TodoRepository에 fetch join 사용하는 코드 @EntityGraph 어노테이션 사용하여 수정

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -3,15 +3,23 @@ package org.example.expert.domain.todo.repository;
 import org.example.expert.domain.todo.entity.Todo;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
 
-    @Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user u ORDER BY t.modifiedAt DESC")
+    //Entity Graph
+    @Override
+    @EntityGraph(attributePaths = {"user"})
+    List<Todo> findAll();
+
+    //Entity Graph
+    @EntityGraph(attributePaths = {"user"})
     Page<Todo> findAllByOrderByModifiedAtDesc(Pageable pageable);
 
     @Query("SELECT t FROM Todo t " +
